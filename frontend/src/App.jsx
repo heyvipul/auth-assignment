4133d97d2dfdf50e453015224ef0126af9655b20
heyvipul <vipulgirhestar@gmail.com>
2024-02-07 16:42:48 +0530

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import axios from "axios"
 import AllRoutes from './Routes/AllRoutes'
 import Navbar from './Routes/Navbar'
-axios.defaults.baseURL = "http://localhost:8000/"
+axios.defaults.baseURL = "https://fair-red-pangolin-yoke.cyclic.app/"
 
 function App() {
 
